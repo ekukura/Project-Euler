9ef3611066471b2cf00fc38fb9cf4c92e6bd8797
ekukura <kukuraemily@gmail.com>
2018-02-06 12:54:13 -0500

    Cleaned Problem 6

--- src/Problem6.py
+++ src/Problem6.py
@@ -15,74 +15,77 @@ sum(i^2, i=1..10) = 385
 sum(i, i=1..10)^2 = 55^2 = 3025
 So diff = 3025-385 = 2640
 '''
-import time
-start = time.time()
 
+import time
+
+def solution_1(n):
 ########################
 # Method 1: Brute Force
 ########################
-n = 600
 
-"""
-def sum_of_squares(max_int):
-    curSum = 0
-    for i in range(max_int):
-        curSum += pow((i+1),2)
-    return curSum
-
-def squared_sum(max_int):
-    curSum = 0
-    for i in range(max_int):
-        curSum += i+1
-    return pow(curSum, 2)
-
-end = time.time()
-print(squared_sum(n)-sum_of_squares(n))
-#"""
-
-########################
-# Method 2: Using Math 
-########################
-#e.g. sum(i^2, i=1..n) - (sum(i, i=1..n)^2) = 2*sum(i*j, i<j<=n)
-
-"""
-curSum = 0
-for j in range(2,n+1):
-    for i in range(1,j):
-        #e.g. when j = 5, want i from 1 to 4
-        curSum += i*j
- 
-end = time.time()       
-print(2*curSum)
-#"""
+    def sum_of_squares(max_int):
+        curSum = 0
+        for i in range(max_int):
+            curSum += pow((i+1),2)
+        return curSum
+    
+    def squared_sum(max_int):
+        curSum = 0
+        for i in range(1,max_int+1):
+            curSum += i
+        return pow(curSum, 2)
+    
+    return squared_sum(n)-sum_of_squares(n)
 
 
+
+def solution_2(n):
 ############################
-# Method 3: Using Recursion 
+# Method 2: Using Recursion 
 ############################
 
-#"""
-def f(max_val):
-    if max_val == 1:
-        return 0
-    else:
-        return f(max_val-1) + pow(max_val, 2)*(max_val-1)
- 
-end = time.time()  
-print(f(n))
-#"""
-        
+#note diff(n) = (1+2+...+ (n-1) + n)^2 - (1^2+2^2+...+(n-1)^2+n^2) 
+# = (1+2+...+(n-1))^2 + n^2 + 2*(1+2+...+(n-1))*n - (1^2+...+(n-1)^2) - n^2
+# = (1+2+...+(n-1))^2 - (1^2+...+(n-1)^2) + 2*(1+2+...+(n-1))*n
+# = diff(n-1) + 2*(n*(n-1))/2 * n = diff(n-1) + n^2*(n-1)
+# (Last line using ID sum(1...m) = m*(m+1)/2
 
+    def diff(max_val):
+        if max_val == 1:
+            return 0
+        else:
+            return diff(max_val-1) + pow(max_val, 2)*(max_val-1)
+     
+    return diff(n)
+
+
+def solution_3(n):
 ############################
 # Method 3: Identities
 ############################
 
-"""
-squared_sum = pow(n*(n+1)/2,2)
-sum_of_squares = n*(n+1)*(2*n+1)/6
-
-end = time.time()  
-print(int(squared_sum - sum_of_squares))
-#"""
-
-print("time:", end-start)  
+    squared_sum = pow(n*(n+1)/2,2)
+    sum_of_squares = n*(n+1)*(2*n+1)/6
+    
+    return squared_sum - sum_of_squares
+    
+      
+if __name__ == '__main__':
+    
+    start = time.time()
+    res_1 = solution_1(100)
+    end = time.time()
+    print("res_1 = {}\nTook {} seconds".format(res_1, end-start)) 
+    
+    start = time.time()
+    res_2 = solution_2(100)
+    end = time.time()
+    print("res_2 = {}\nTook {} seconds".format(res_2, end-start))   
+    
+    start = time.time()
+    res_3 = solution_3(100)
+    end = time.time()
+    print("res_3 = {}\nTook {} seconds".format(res_3, end-start)) 
+    
+    #Answer: 25164150
+    
