95e29356b0eea9430fc46e6046a454cc4ebf9ffd
ekukura <kukuraemily@gmail.com>
2018-02-07 11:46:30 -0500

    Cleaned Problem 7

--- src/Problem7.py
+++ src/Problem7.py
@@ -14,6 +14,7 @@ What is the 10 001st prime number?
 
 import math, time
 
+
 def isPrime(x):
     foundComposite = False
     if x == 2:
@@ -26,77 +27,57 @@ def isPrime(x):
             #testing all possible lower divisors
             if x % i == 0:
                 foundComposite = True
-            i += 1
+            i += 2
         return not foundComposite
 
-start = time.time()
-
+def solution_1(primeIndex):
 #### Solution 1, plain brute force #######
-#"""
-counter = 2
-primeIndex = 10001
-#while counter <= primeIndex :
-'''
-i = 2
-while True:
-    if isPrime(i):
-        counter += 1
-    if counter == primeIndex:
-        break
-    i += 1
-'''
-#all primes greater than 2 are odd
-i = 3
-while counter < primeIndex:
-    i+=2
-    if isPrime(i):
-        counter += 1
-        
-print(i)
+    counter = 2
 
-#10001st prime:
-#104743
-#In time: 1.3114359378814697 seconds
-#"""
-
-###### Solution 2, modified brute force ########
-"""
-primeIndex = 25
-'''
-#does from primeIndex > 1 (1st prime is 2)
-counter = 2
-i = 3
-while counter < primeIndex:
-    i+=2 #don't need to test even numbers past 2 
-    if i % 3 == 0:
-        pass
-    elif i > 5 and i % 5 == 0:
-        pass
-    elif i > 7 and i % 7 == 0:
-        pass
-    elif i > 11 and i % 11 == 0:
-        pass
-    elif isPrime(i):
-        counter += 1
-'''
-      
-#does from primeIndex > 5 (6th prime is 13)
-counter = 6
-i = 13
-while counter < primeIndex:
-    i+=2 #don't need to test even numbers past 2 
-    if i % 3 == 0 or i % 5 == 0 or i % 7 == 0 or i % 11 == 0 or i % 13 == 0:
-        pass
-    else:
-        print(i)
+    #all primes greater than 2 are odd
+    i = 3
+    while counter < primeIndex:
+        i+=2
         if isPrime(i):
             counter += 1
-#"""        
-print(time.time()-start)
+            
+    return i
+
+def solution_2(primeIndex):
+###### Solution 2, modified brute force ########
+
+    #does from primeIndex > 2 (1st two primes are 2,3)
+    counter = 2
+    i = 3
+    while counter < primeIndex:
+        i+=2 #don't need to test even numbers past 2 
+        if i % 3 == 0:
+            pass
+        elif i > 5 and i % 5 == 0:
+            pass
+        elif i > 7 and i % 7 == 0:
+            pass
+        elif i > 11 and i % 11 == 0:
+            pass
+        elif isPrime(i):
+            counter += 1
+
+    return i
+
+
+if __name__ == '__main__':  
     
-'''
-print(7, isPrime(7))
-print(8, isPrime(8))
-print(51, isPrime(51))
-print(31, isPrime(31))
-#'''               
+    start = time.time()
+    res_1 = solution_1(10001)
+    end = time.time()
+    print("res_1 = {}\nTook {} seconds".format(res_1, end-start))  
+    
+    start = time.time()
+    res_2 = solution_2(10001)
+    end = time.time()
+    print("res_2 = {}\nTook {} seconds".format(res_2, end-start))  
+    
+    #Answer: 104743
+   
+ 
+
