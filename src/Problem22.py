c7153df2b86b8fd830eee564cb29333883e72002
ekukura <kukuraemily@gmail.com>
2018-02-10 15:32:23 -0500

    Organized Problem 22 with 'main'

--- src/Problem22.py
+++ src/Problem22.py
@@ -11,63 +11,55 @@ Then working out the alphabetical value for each name, multiply this value by it
 alphabetical position in the list to obtain a name score.
 
 For example, when the list is sorted into alphabetical order, COLIN,
- which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN would obtain a score of 938 × 53 = 49714.
+ which is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. 
+ So, COLIN would obtain a score of 938 × 53 = 49714.
 
 What is the total of all the name scores in the file?
-
 """
 
 import time
 
-name_file = open("p022_names.txt", "r")
-
-names = sorted(name_file.readline().split(','))
-num_names = len(names)
-
-letters_num = [i for i in range(1,27)]
-letters = [chr(letters_num[i] + 64) for i in range(len(letters_num))]
-#print(letters)
-value_dict = {letters[i]:letters_num[i] for i in range(len(letters_num))}
-print(value_dict)
-print()
-
-def get_name_score(name, pos): #name a string
-    name_chars = list(name)
-    #print(name_chars)
-    score = 0
+def get_name_score(value_dict, name, pos): #name a string
+    name_chars = list(name) #e.g. takes 'ALEX' to ['A', 'L', 'E', 'X']
+    pre_pos_score = 0
     for i in range(len(name_chars)):
-        score += pos * value_dict[name_chars[i]]
-        #print(score)
+        pre_pos_score += value_dict[name_chars[i]]
         
+    return pos* pre_pos_score
+
+
+def solution_1(name_file):
+    
+    names = sorted(name_file.readline().split(','))
+    #print(names)
+    letters_index = [i for i in range(1, 27)]
+    letters = [chr(letters_index[i] + 64) for i in range(len(letters_index))]
+    #print(letters)
+    value_dict = {letters[i]:letters_index[i] for i in range(len(letters_index))}
+    #print(value_dict)
+  
+    score = 0 #49, 70, 57 first three scores
+    for i in range(1, len(names) + 1): 
+        #names list of form ['"name1"', '"name2"', ... ] so need to remove the surrounding "'s
+        cur_name = names[i-1].replace("\"", "")  #e.g. takes '"name1"' to 'name1'
+        current_score = get_name_score(value_dict, cur_name, i)
+        score += current_score
+    
     return score
 
-start = time.time()
-score = 0 #49, 70, 57 first three scores
-for i in range(len(names)):
-#for i in range(3):
-    cur_name = names[i].replace("\"","")
-    current_score = get_name_score(cur_name, i+1)
-    score += current_score
 
-res = score
-end = time.time()
-print("The total score is:", score)
-print("Took {} seconds".format(end-start))
+if __name__ == '__main__':
+    
+    name_file = open("p022_names.txt", "r")
+    
+    start = time.time()
+    res_1 = solution_1(name_file)
+    end = time.time()
+    print("res_1 = {}\nTook {} seconds".format(res_1, end-start))  
+        
+    name_file.close()
+    
+    # Answer: 871198282
 
 
 
-'''
-name = names[0]
-print(list(name.replace("\"", "")))
-print(name)
-name_chars = list(name)
-print(name_chars)
-print(num_names)
-print(names[0])
-print(names[0][1])
-print(ord(names[0][1]))
-print(names[num_names - 1])
-print(names[937])
-'''
-
-name_file.close()
