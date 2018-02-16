40d97c5fc107f002eca6bc260d521dbbe670fdba
ekukura <kukuraemily@gmail.com>
2018-02-16 12:04:56 -0500

    Organized Problem 28 with 'main'

--- src/Problem28.py
+++ src/Problem28.py
@@ -23,41 +23,48 @@ formed in the same way?
 
 import time
 
-def sum_diagonals_spiral(odd):
-    k = int((odd-1)/2)
+def solution_1(odd):
+    k = int((odd-1)/2) #this is the number of nested squares, excluding the center 1
     if k == 0:
         total_diagonals_sum = 1
     else:
-        NE_sum = sum([pow(2*i+1,2) for i in range(1,k+1)])
-        SE_sum = sum([pow(2*i,2) - (2*i-1) for i in range(1,k+1)])
-        SW_sum = sum([pow(2*i,2) + 1 for i in range(1,k+1)])
-        NW_sum = sum([pow(2*i+1,2) - 2*i for i in range(1,k+1)])
-        total_diagonals_sum = NE_sum + SE_sum + SW_sum + NW_sum + 1
+        NE_sum = sum([pow(2*i+1,2) for i in range(1,k+1)]) #upper right quadrant
+        SE_sum = sum([pow(2*i,2) - (2*i-1) for i in range(1,k+1)]) #lower right quadrant
+        SW_sum = sum([pow(2*i,2) + 1 for i in range(1,k+1)]) #lower left quadrant
+        NW_sum = sum([pow(2*i+1,2) - 2*i for i in range(1,k+1)]) #upper left quadrant
+        total_diagonals_sum = NE_sum + SE_sum + SW_sum + NW_sum + 1  #1 takes care of middle element
         
     return total_diagonals_sum
 
-def sum_opt(odd):
+def solution_2(odd):
     k = int((odd-1)/2)
     if k == 0:
         total_diagonals_sum = 1
     else:
-        #total_diagonals_sum = 2 * sum([pow(2*i+1,2) + pow(2*i,2) - 2*i + 1 for i in range(1,k+1)]) + 1  
-        #total_diagonals_sum = 2 * sum([8*pow(i,2) + 2*i + 2 for i in range(1,k+1)]) + 1     
-        #total_diagonals_sum = (16 * sum([pow(i,2) for i in range(1,k+1)]) + 
-        #                       4 * sum([i for i in range(1, k+1)]) + 4*k + 1)
-        total_diagonals_sum = int(k*(k+1)*( (8/3)*(2*k+1) + 2 ) + 4*k + 1)
-        # this final formula uses closed form for sum(i: i=1:k) = k(k+1)/2
+        # derivation for formula: 
+        # total_diagonals_sum = 2 * sum([pow(2*i+1,2) + pow(2*i,2) - 2*i + 1 for i in range(1,k+1)]) + 1 
+        #    = 2 * sum( (4*i^2 + 4*i + 1) + 4*i^2 - 2*i + 1) for i in range(1, k+1) ) + 1
+        #    = 2 * sum( (8*i^2) + 2*i + 2 for i in range(1,k+1) ) + 1     
+        #    = (16 * sum( (i^2) : i=1:k) + 4 * sum(i: i=1:k) + 4*k + 1)
+        #    = 16 * k(k+1)(2k+1)/6 + 4 * k(k+1)/2 + 4*k + 1
+        # (last formula uses closed form for sum(i: i=1:k) = k(k+1)/2
         # and sum(i^2: i=1:k) = k(k+1)(2k+1)/6
+        total_diagonals_sum = int(k*(k+1)*( (8/3)*(2*k+1) + 2 ) + 4*k + 1)
         
     return total_diagonals_sum
-        
-num = 1001
-start = time.time()
-res = sum_diagonals_spiral(num)
-end = time.time()
-print("The sum is {}\nTook {} seconds".format(res, end-start))
-start = time.time()
-res2 = sum_opt(num)
-end = time.time()
-print("The sum is {}\nTook {} seconds".format(res2, end-start))
 
+if __name__ == "__main__":
+
+    num = 1001
+    
+    start = time.time()
+    res_1 = solution_1(num)
+    end = time.time()
+    print("The sum is {}\nTook {} seconds".format(res_1, end-start))
+    
+    start = time.time()
+    res_2 = solution_2(num)
+    end = time.time()
+    print("The sum is {}\nTook {} seconds".format(res_2, end-start))
+    
+    #Answer: 669171001
